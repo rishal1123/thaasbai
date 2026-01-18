b09572e922d34d85a53154755f121e8fd8cc1625
mohamed.risal <mohamed.risal@hdc.mv>
2026-01-18 12:48:47 +0500

    Make whole Ooredoo area clickable with padding

--- service-worker.js
+++ service-worker.js
@@ -4,7 +4,7 @@
  */
 
 // IMPORTANT: Increment this version to trigger update
-const CACHE_VERSION = 41;
+const CACHE_VERSION = 42;
 const CACHE_NAME = `thaasbai-v${CACHE_VERSION}`;
 const OFFLINE_URL = '/';
 
