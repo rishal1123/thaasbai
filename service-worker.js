0e5c9c26464a7baded17e38856ee779c52530f65
mohamed.risal <mohamed.risal@hdc.mv>
2026-01-18 13:07:32 +0500

    Revert food sponsor size back to 16x20

--- service-worker.js
+++ service-worker.js
@@ -4,7 +4,7 @@
  */
 
 // IMPORTANT: Increment this version to trigger update
-const CACHE_VERSION = 45;
+const CACHE_VERSION = 46;
 const CACHE_NAME = `thaasbai-v${CACHE_VERSION}`;
 const OFFLINE_URL = '/';
 
